676db1acecc3e24f8a00c6c5c742e6d63329393c
KorirMoze <korirkipsmoze@gmail.com>
2023-04-04 23:21:37 +0300

    tuesday

--- src/pages/layout/dashBoard.vue
+++ src/pages/layout/dashBoard.vue
@@ -1,9 +1,107 @@
 <template>
-    <div>dashBoard</div>
+    <div class="pop">
+        <div class="popular">
+            <h2>Popular Games</h2>
+        </div>
+        <div class="popgame">
+            <div class="max-w-sm rounded overflow-hidden shadow-lg tt">
+                <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
+                <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+                <p class="text-gray-700 text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                </p>
+                </div>
+                <div class="px-6 pt-4 pb-2">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                </div>
+            </div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg tt">
+                <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
+                <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+                <p class="text-gray-700 text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                </p>
+                </div>
+                <div class="px-6 pt-4 pb-2">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                </div>
+            </div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg tt">
+                <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
+                <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+                <p class="text-gray-700 text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                </p>
+                </div>
+                <div class="px-6 pt-4 pb-2">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="all_games">
+    <div class="rounded overflow-hidden shadow-lg match">
+        <!-- <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"> -->
+        <div class="px-6 py-4">
+        <div class="text-gray mb-2 hometeam"><span>Home Team</span> <span>home</span></div>
+        
+        <div class="text-gray mb-2">Away Team</div>
+        <p class="text-gray-700 text-base">
+        </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#home</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Draw</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Away</span>
+        </div>
+    </div>
+</div>
 </template>
 <script>
 export default {
 
 }
 </script>
-<style></style>
+<style>
+.pop{
+    display: block;
+    margin: auto;
+    width: 60%;
+}
+.popular{
+    background-color: #1A5F7A;
+    font-weight: 700;
+    text-align: left;
+    padding: 0.5em 0.75em;
+    display: block;
+    margin: auto;
+    width: 100%;
+}
+.popgame{
+    display: flex;
+    flex-direction: horizontal;
+}
+.tt{
+    margin-right: 10px;
+}
+.all_games{
+    width: 60%;
+    margin: auto;
+    text-align: left !important;
+}
+.hometeam {
+    justify-content: space-between;
+}
+.match {
+    width: 100%;
+}
+</style>
