4202c6fcb14c0ed3b0dc97e3ac7b256da77d33cb
KorirMoze <korirkipsmoze@gmail.com>
2023-04-25 17:03:35 +0300

    data being send to database

--- src/components/betterSlip.vue
+++ src/components/betterSlip.vue
@@ -39,29 +39,59 @@ export default {
       this.$emit('update:betslip', updatedBetslip);
       this.betslipKey += 1;
     },
-    placeBet() {
-      console.log('Placing bets:', this.betslip);
+//     placeBet() {
+//       console.log('Sending request with data:', {
+//       match: this.betslip.match,
+//       selection: this.betslip.selection,
+//       odds: this.betslip.odds,
+//       stake: this.betslip.stake
+//     });
+//     console.log('Placing bets:', this.betslip);
 
-      // Loop through the betslip array and send each item as a separate request
-      for (let i = 0; i < this.betslip.length; i++) {
-        const bet = this.betslip[i];
+//     axios.post('http://127.0.0.1:8000/place_bet/', {
+//       match: this.betslip.match,
+//       selection: this.betslip.selection,
+//       odds: this.betslip.odds,
+//       stake: this.betslip.stake
+//     })
+//     .then(response => {
+//       console.log('Bet placed:', response.data);
+//       // Emit a "bet-placed" event to update the UI
+//       this.$emit('bet-placed', response.data.bet);
+//     })
+//     .catch(error => {
+//       console.error('Failed to place bet:', error);
+//     });
 
-        // Send a POST request to the backend API for this bet
-        axios.post('http://127.0.0.1:8000/place_bet/', {
-          match: bet.match,
-          selection: bet.selection,
-          odds: bet.odds,
-          stake: bet.stake
-        })
-        .then(response => {
-          console.log('Bet placed:', response.data);
-          // Emit a "bet-placed" event to update the UI
-          this.$emit('bet-placed', response.data.bet);
-        })
-        .catch(error => {
-          console.error('Failed to place bet:', error);
-        });
-      }
+// }
+placeBet() {
+  // console.log('Placing bets:', this.betslip);
+
+  // Loop through the betslip array and send each item as a separate request
+  for (let i = 0; i < this.betslip.length; i++) {
+    const bet = this.betslip[i];
+    console.log(bet.odds)
+
+    // Send a POST request to the backend API for this bet
+    axios.post('http://127.0.0.1:8000/place_bet/', {
+      match: bet.match,
+      
+      selection: bet.selection,
+      odds: bet.odds,
+      stake: bet.stake
+    })
+    .then(response => {
+      console.log('Bet placed:', response.data);
+      // console.log('Bet ed:', bet.stake);
+      // Emit a "bet-placed" event to update the UI
+      this.$emit('bet-placed', response.data.bet);
+    })
+    .catch(error => {
+      console.error('Failed to place bet:', error);
+    });
+  }
+}
+
 
   },
   watch: {
