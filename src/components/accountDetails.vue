ca2a53f14bd8dd1ad19ab7296c3acdd0568d0f10
KorirMoze <korirkipsmoze@gmail.com>
2023-05-03 18:14:24 +0300

    manage to display the account data

--- src/components/accountDetails.vue
+++ src/components/accountDetails.vue
@@ -1,43 +1,62 @@
 <template>
     <div>
       <h1>Account Details</h1>
-      <p>Email: {{ gambler.email }}</p>
-      <p>First Name: {{ gambler.first_name }}</p>
-      <p>Last Name: {{ gambler.last_name }}</p>
-      <p>Date of Birth: {{ gambler.date_of_birth }}</p>
-      <p>Gender: {{ gambler.gender }}</p>
-      <p>Phone Number: {{ gambler.phone_number }}</p>
-      <p>Account Balance: {{ gambler.acc_balance }}</p>
+      <template v-if="Object.keys(gambler).length !== 0">
+        <pre>{{ JSON.stringify(gambler, null, 2) }}</pre>
+        <p>Email: {{ gambler.data && gambler.data.email }}</p>
+        <p>First Name: {{ gambler.data && gambler.data.first_name }}</p>
+        <p>Last Name: {{ gambler.data && gambler.data.last_name }}</p>
+        <p>Date of Birth: {{ gambler.data && gambler.data.date_of_birth }}</p>
+        <p>Gender: {{ gambler.data && gambler.data.gender }}</p>
+        <p>Phone Number: {{ gambler.data && gambler.data.phone_number }}</p>
+        <p>Account Balance: {{ gambler.data && gambler.data.acc_balance }}</p>
+      </template>
+      <template v-else>
+        <p>Loading...</p>
+      </template>
     </div>
   </template>
   
-  <script>
-    import axios from 'axios';
   
-    export default {
-      data() {
-        return {
-          gambler: {},
-        };
-      },
-      mounted() {
-        const token = localStorage.getItem('jwt');
-        if (token) {
-          axios.get('http://127.0.0.1:8000/account_details/', {
+  <script>
+  import axios from "axios";
+  
+  export default {
+    data() {
+  return {
+    gambler: {
+      email: '',
+      first_name: '',
+      last_name: '',
+      date_of_birth: '',
+      gender: '',
+      phone_number: '',
+      acc_balance: '',
+    },
+  };
+},
+    mounted() {
+      const token = localStorage.getItem("jwt");
+      console.log(token)
+      if (token) {
+        axios
+          .get("http://127.0.0.1:8000/account_details/", {
             headers: {
               Authorization: `Bearer ${token}`,
             },
           })
-            .then((response) => {
-              this.gambler = response.data;
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-        } else {
-          console.log('No token found');
-        }
-      },
-    };
+          .then((response) => {
+            console.log(response.data);
+            this.gambler = response.data;
+            console.log(this.gambler);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      } else {
+        console.log("No token found");
+      }
+    },
+  };
   </script>
   
