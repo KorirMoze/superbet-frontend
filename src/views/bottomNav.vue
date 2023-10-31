b4a5a9152ba87ba4f5210de31eacbf28e545d0eb
KorirMoze <korirkipsmoze@gmail.com.com>
2023-10-31 07:51:03 +0300

    bottom nav

---
+++ src/views/bottomNav.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="bottomnav">
+   
+            <a class="nav-link" href="/login">Login</a>
+     
+            <a class="nav-link" href="/account">My Account</a>
+
+            <a class="nav-link">Your Component</a>
+       
+            <router-link to="/registration" class="nav-link">Registration</router-link>
+         
+     
+            <a class="nav-link" href="#responsive-header">Logout</a>
+        
+    </div>
+</template>
+<style scoped>
+.bottomnav{
+    background-color: #45474B;
+    color: #F4CE14;
+    display: flex;
+    justify-content: space-between;
+}
+
+</style>
